70b7cbaaa1288f9144d556ac3b590d46d712729e
Alex Yong <alexjyong@gmail.com>
2023-08-30 14:34:27 +0000

    stashing changes.

--- BookBard/App.js
+++ BookBard/App.js
@@ -1,91 +1,52 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text, ScrollView, TextInput } from 'react-native';
-import PDFView from 'react-native-pdf';
-//import { Reader, ReaderProvider } from '@epubjs-react-native/core';
-import { request, PERMISSIONS } from 'react-native-permissions';
+import { View, Text, Button, Alert } from 'react-native';
+import ePub from 'epubjs';
+import RNFS from 'react-native-fs';
 import DocumentPicker from 'react-native-document-picker';
 
 const App = () => {
-  const [fileUri, setFileUri] = useState(null);
-  const [fileType, setFileType] = useState(null);
-  const [logs, setLogs] = useState([]);
+  const [epubText, setEpubText] = useState('');
 
-  const addLog = (message) => {
-    setLogs((prevLogs) => [...prevLogs, message]);
-  };
-
-  const selectFile = async () => {
+  const pickDocument = async () => {
     try {
-      const result = await DocumentPicker.pickSingle({
-        type: [DocumentPicker.types.pdf, DocumentPicker.types.plainText],
+      const result = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
       });
-      setFileUri(result.uri);
-      setFileType(result.type === 'application/pdf' ? 'pdf' : 'epub');
-      addLog(`Selected file from: ${result.uri}`);
+
+      if (result) {
+        extractEpubText(result.uri);
+      }
     } catch (err) {
       if (DocumentPicker.isCancel(err)) {
-        addLog('File selection was cancelled.');
+        Alert.alert('User cancelled the picker');
       } else {
-        addLog(`Error selecting file: ${err.message}`);
         throw err;
       }
     }
   };
 
+  const extractEpubText = async (filePath) => {
+    // Read the EPUB file
+    const data = await RNFS.readFile(filePath, 'base64');
+    const epubData = `data:application/epub+zip;base64,${data}`;
+    const book = ePub(epubData);
+
+    // Get the first section (as an example)
+    const section = book.spine.get(0);
+    const contents = await section.load();
+    const text = contents.textContent;
+
+    // Set the extracted text to state
+    setEpubText(text);
+  };
+
   return (
-    <View style={styles.container}>
-      <Button title="Select File" onPress={selectFile} />
-      {fileType === 'pdf' && (
-        <PDFView
-          source={{ uri: fileUri, cache: true }}
-          style={styles.pdfView}
-        />
-      )}
-      {/* {fileType === 'epub' && (
-        <ReaderProvider>
-          <Reader
-            src={fileUri}
-            width={300}
-            height={400}
-          />
-        </ReaderProvider>
-      )} */}
-      <ScrollView style={styles.logView}>
-        <TextInput
-          style={{ height: '100%' }}
-          multiline={true}
-          editable={true}
-          onChangeText={(text) => {
-            if (text !== logs.join('\n')) {
-              setLogs(logs);
-            }
-          }}
-          value={logs.join('\n')}
-        />
-      </ScrollView>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Extracted EPUB Text:</Text>
+      <Text>{epubText}</Text>
+      <Button title="Pick EPUB File" onPress={pickDocument} />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  pdfView: {
-    width: 300,
-    height: 400,
-    marginBottom: 20,
-  },
-  logView: {
-    marginTop: 20,
-    width: '80%',
-    height: 100,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-  },
-});
+};
 
 export default App;
