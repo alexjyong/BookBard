4431348587a0257afe3ca8c31301a0823993cb64
Alex Yong <alexjyong@gmail.com>
2023-11-30 21:31:57 -0500

    Update index.js

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -25,7 +25,7 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    
     alert("Ready!");
 }
 
